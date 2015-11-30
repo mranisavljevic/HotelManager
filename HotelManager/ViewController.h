fecdbecd2bf1195a638ad88ae83b9c71484a3918
Miles Ranisavljevic <itstop3@gmail.com>
2015-11-30 12:29:24 -0800

    Initial commit

---
+++ HotelManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HotelManager
+//
+//  Created by Miles Ranisavljevic on 11/30/15.
+//  Copyright © 2015 creeperspeak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
