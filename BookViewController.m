0e83f9c81d18e3cb29a15d424b0d36d0c182d021
Miles Ranisavljevic <itstop3@gmail.com>
2015-12-01 21:35:56 -0800

    Add startDatePicker method to automatically advance endDate when start date is greater than end, or when end is more than 30 days greater than start.

--- BookViewController.m
+++ BookViewController.m
@@ -19,7 +19,7 @@
 
 - (void)loadView {
     [super loadView];
-    [self.view setBackgroundColor:[UIColor colorWithRed:0.333 green:0.329 blue:0.329 alpha:1.000]];
+    [self.view setBackgroundColor:[UIColor colorWithWhite:0.875 alpha:1.000]];
 }
 
 - (void)viewDidLoad {
@@ -48,18 +48,20 @@
     self.startDatePicker.datePickerMode = UIDatePickerModeDate;
     self.endDatePicker.datePickerMode = UIDatePickerModeDate;
     
+    [self.startDatePicker addTarget:self action:@selector(startPickerValueChanged:) forControlEvents:UIControlEventValueChanged];
+    
     self.startDatePicker.minimumDate = [NSDate date];
     self.endDatePicker.minimumDate = [NSDate dateWithTimeInterval:86400 sinceDate:self.startDatePicker.date];
     
     float navBarHeight = self.navigationController.navigationBar.frame.size.height;
     float halfAvailableViewHeight = (self.view.frame.size.height - navBarHeight - 40.0) / 2;
     
-    NSLayoutConstraint *startTop = [NSLayoutConstraint constraintWithItem:self.startDatePicker attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1.0 constant:navBarHeight + 20.0];
+    NSLayoutConstraint *startTop = [NSLayoutConstraint constraintWithItem:self.startDatePicker attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1.0 constant:navBarHeight + 40.0];
     NSLayoutConstraint *startLeading = [NSLayoutConstraint constraintWithItem:self.startDatePicker attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeLeadingMargin multiplier:1.0 constant:0.0];
     NSLayoutConstraint *startTrailing = [NSLayoutConstraint constraintWithItem:self.startDatePicker attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTrailingMargin multiplier:1.0 constant:0.0];
     NSLayoutConstraint *startHeight = [NSLayoutConstraint constraintWithItem:self.startDatePicker attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0 constant:halfAvailableViewHeight];
     
-    NSLayoutConstraint *endTop = [NSLayoutConstraint constraintWithItem:self.endDatePicker attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.startDatePicker attribute:NSLayoutAttributeBottom multiplier:1.0 constant:20.0];
+    NSLayoutConstraint *endTop = [NSLayoutConstraint constraintWithItem:self.endDatePicker attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.startDatePicker attribute:NSLayoutAttributeBottom multiplier:1.0 constant:0.0];
     NSLayoutConstraint *endLeading = [NSLayoutConstraint constraintWithItem:self.endDatePicker attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeLeadingMargin multiplier:1.0 constant:0.0];
     NSLayoutConstraint *endTrailing = [NSLayoutConstraint constraintWithItem:self.endDatePicker attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTrailingMargin multiplier:1.0 constant:0.0];
     NSLayoutConstraint *endHeight = [NSLayoutConstraint constraintWithItem:self.endDatePicker attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0 constant:halfAvailableViewHeight];
@@ -85,19 +87,19 @@
     startLabel.text = @"Check In:";
     endLabel.text = @"Check Out:";
     
-    startLabel.textColor = [UIColor whiteColor];
-    endLabel.textColor = [UIColor whiteColor];
+    startLabel.textColor = [UIColor blackColor];
+    endLabel.textColor = [UIColor blackColor];
     
-    startLabel.font = [UIFont fontWithName:@"Papyrus" size:20];
-    endLabel.font = [UIFont fontWithName:@"Papyrus" size:20];
+    startLabel.font = [UIFont fontWithName:@"Papyrus" size:30];
+    endLabel.font = [UIFont fontWithName:@"Papyrus" size:30];
     
     startLabel.translatesAutoresizingMaskIntoConstraints = NO;
     endLabel.translatesAutoresizingMaskIntoConstraints = NO;
     
-    NSLayoutConstraint *startLabelTop = [NSLayoutConstraint constraintWithItem:startLabel attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.startDatePicker attribute:NSLayoutAttributeTop multiplier:1.0 constant:40.0];
+    NSLayoutConstraint *startLabelTop = [NSLayoutConstraint constraintWithItem:startLabel attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.startDatePicker attribute:NSLayoutAttributeTop multiplier:1.0 constant:50.0];
     NSLayoutConstraint *startLabelCenterX = [NSLayoutConstraint constraintWithItem:startLabel attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.startDatePicker attribute:NSLayoutAttributeCenterX multiplier:1.0 constant:0.0];
     
-    NSLayoutConstraint *endLabelTop = [NSLayoutConstraint constraintWithItem:endLabel attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.endDatePicker attribute:NSLayoutAttributeTop multiplier:1.0 constant:40.0];
+    NSLayoutConstraint *endLabelTop = [NSLayoutConstraint constraintWithItem:endLabel attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.endDatePicker attribute:NSLayoutAttributeTop multiplier:1.0 constant:50.0];
     NSLayoutConstraint *endLabelCenterX = [NSLayoutConstraint constraintWithItem:endLabel attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.endDatePicker attribute:NSLayoutAttributeCenterX multiplier:1.0 constant:0.0];
     
     [self.view addSubview:startLabel];
@@ -109,6 +111,12 @@
     endLabelTop.active = YES;
     endLabelCenterX.active = YES;
     
+}
+
+- (void)startPickerValueChanged:(UIDatePicker *)sender {
+    if (self.endDatePicker.date <= self.startDatePicker.date || floorf([self.endDatePicker.date timeIntervalSinceDate:self.startDatePicker.date] / 60 / 60 / 24) > 30) {
+        [self.endDatePicker setDate:[NSDate dateWithTimeInterval:86400 sinceDate:self.startDatePicker.date] animated:YES];
+    }
 }
 
 - (void)doneButtonPressed:(UIBarButtonItem *)sender {
